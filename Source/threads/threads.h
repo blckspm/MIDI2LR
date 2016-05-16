7fb8e8571c754b345beef2f030a29b5e501a018e
Rory Jaffe <rsjaffe@gmail.com>
2016-05-15 20:10:22 -0700

    more thread code

--- Source/threads/threads.h
+++ Source/threads/threads.h
@@ -71,3 +71,91 @@ void consumer(int num)
         std::cout << "consumer " << num << ": " << val << std::endl;
     }
 }
+
+class interrupt_flag
+{
+    std::atomic<bool> flag;
+    std::condition_variable* thread_cond;
+    std::mutex set_clear_mutex;
+public:
+    interrupt_flag():
+        thread_cond(0)
+    {}
+    void set()
+    {
+        flag.store(true, std::memory_order_relaxed);
+        std::lock_guard<std::mutex> lk(set_clear_mutex);
+        if (thread_cond)
+        {
+            thread_cond->notify_all();
+        }
+    }
+    bool is_set() const
+    {
+        return flag.load(std::memory_order_relaxed);
+    }
+    void set_condition_variable(std::condition_variable& cv)
+    {
+        std::lock_guard<std::mutex> lk(set_clear_mutex);
+        thread_cond = &cv;
+    }
+    void clear_condition_variable()
+    {
+        std::lock_guard<std::mutex> lk(set_clear_mutex);
+        thread_cond = 0;
+    }
+    struct clear_cv_on_destruct
+    {
+        ~clear_cv_on_destruct()
+        {
+            this_thread_interrupt_flag.clear_condition_variable();
+        }
+    };
+};
+
+template<typename Predicate>
+void interruptible_wait(std::condition_variable& cv,
+    std::unique_lock<std::mutex>& lk,
+    Predicate pred)
+{
+    interruption_point();
+    this_thread_interrupt_flag.set_condition_variable(cv);
+    interrupt_flag::clear_cv_on_destruct guard;
+    while (!this_thread_interrupt_flag.is_set() && !pred())
+    {
+        cv.wait_for(lk, std::chrono::milliseconds(1));
+    }
+    interruption_point();
+}
+
+
+class interruptible_thread
+{
+    std::thread internal_thread;
+    interrupt_flag* flag;
+public:
+    template<typename FunctionType>
+    interruptible_thread(FunctionType f)
+    {
+        std::promise<interrupt_flag*> p;
+        internal_thread = std::thread([f, &p]
+        {
+            p.set_value(&this_thread_interrupt_flag);
+            try
+            {
+                f();
+            }
+            catch (thread_interrupted const&)
+            {
+            }
+        });
+        flag = p.get_future().get();
+    }
+    void interrupt()
+    {
+        if (flag)
+        {
+            flag->set();
+        }
+    }
+};
